f15e5517b3229620bb2ef35a21bb6ad6fe4ccd64
Simoneeeeeeeeee <forti.simone@iisgalvanimi.edu.it>
2022-10-21 09:36:24 +0000

    versione2

---
+++ esercizio2.py
@@ -0,0 +1,8 @@
+thislist = "apple","banana","cherry"
+print(thislist)
+#creo una lista e stampo la quantita
+thislist = ["ciao", "banana", "bella"]
+print(len(thislist))
+#stampo il tipo di lista
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
